0a3e88c23e16594ed5cac8b8425f8201c748f0b5
VladimirsKrasikovs <108757262+VladimirsKrasikovs@users.noreply.github.com>
2022-07-18 21:20:52 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Dblogic db = new Dblogic();
+
+       //db.insert();
+        //db.delete();
+       // db.update();
+        db.select();
+
+    }
+
+    }
